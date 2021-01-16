a8f2ad8306bab6f8366a50cd77568fac66e0cfb7
[Alwin] <[alwinnocom@gmail.com]>
2021-01-15 18:33:07 -0800

    3rd commit - 1/15 - Angular works when directives correctly have * attached to them. NgForm is buggy.

--- src/app/costs/costs.component.ts
+++ src/app/costs/costs.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './costs.component.html',
   styleUrls: ['./costs.component.css']
 })
+
 export class CostsComponent implements OnInit {
 
   constructor() { }
@@ -12,7 +13,7 @@ export class CostsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  @ViewChild('f') signupForm: NgForm;
+  // @ViewChild('f') addCostsForm: NgForm;
 
   costs: { [key: string]: number } = { 'Food': 0, 'Hotel/Lodging': 0, 'Airfare': 0, 'Taxi': 0, 'Shopping': 0, 'Personal Expenses': 0, 'Other': 0 };
 
