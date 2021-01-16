278ef17ee431563d5ed602050aba005ec55d84d7
[Alwin] <[alwinnocom@gmail.com]>
2021-01-15 17:25:21 -0800

    2nd commit - 1/15 - costs, Datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { MaterialModule } from './material.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
     MaterialModule
   ],
   providers: [],
--- src/app/costs/costs.component.ts
+++ src/app/costs/costs.component.ts
@@ -12,8 +12,20 @@ export class CostsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @ViewChild('f') signupForm: NgForm;
 
   costs: { [key: string]: number } = { 'Food': 0, 'Hotel/Lodging': 0, 'Airfare': 0, 'Taxi': 0, 'Shopping': 0, 'Personal Expenses': 0, 'Other': 0 };
 
+  totalCost: number = 0;
+
+  onSubmit() {
+
+  }
+
+  ngOnChanges() {
+    for (const [key, value] of Object.entries(this.costs)) {
+      this.totalCost += value;
+    }
+  }
 
 }
