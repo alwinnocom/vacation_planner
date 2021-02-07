c82bccd899a2d77a2778c32421433d9f1ac92523
[Alwin] <[alwinnocom@gmail.com]>
2021-02-06 18:46:53 -0800

    2/6 - 3rd commit - Add footer, add dynamic grid-list-tiles for Testimonial section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Tile } from '../models/home-tile.model';
 
 @Component({
   selector: 'app-home',
@@ -29,5 +30,14 @@ export class HomeComponent implements OnInit {
   }
 
   imgPath: string = "assets/images"
+
+  tiles: Tile[] = [
+    {text: "Picture of generated person's face", cols: 1, rows:1, color: 'lightblue'},
+    {text: "If I were a real person, I would give a 5-star rating for 5-star planning!", cols: 2, rows:1, color: 'lightgreen'},
+    {text: "I am also not a real person, but this app is really good.", cols: 2, rows:1, color: 'lightpink'},
+    {text: "Picture of generated person's face", cols: 1, rows:1, color: 'lightblue'},
+    {text: "Picture of generated person's face", cols: 2, rows:1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows:1, color: 'lightpink'}
+  ];
 
 }
---
+++ src/app/models/home-tile.model.ts
@@ -0,0 +1,7 @@
+// Model from Angular Material Documentation for Grid-List-Tile = https://material.angular.io/components/grid-list/examples
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
