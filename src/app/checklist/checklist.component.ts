cef9f419cfb43afa2440f0ecb6371b7fcbce9a5a
[Alwin] <[alwinnocom@gmail.com]>
2021-02-07 17:33:12 -0800

    2/7 - 7th commit - Tabs for Desktop version of checklist + Accordion for Mobile

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -47,9 +47,9 @@ export class ChecklistComponent implements OnInit {
   }
 
   public items: Checklist[] = [
-    { name: "Essentials", iconName: "announcement", subcategories: ['Toiletries', 'Clothes', 'Wallet', 'Phone', 'Credit Card', 'Cash'] },
-    { name: "Airport", iconName: "flight_takeoff", subcategories: ['Alarm', 'Passport', "Driver's License", 'Luggage', 'Ticket'] },
-    { name: "Clothes", iconName: "snowshoeing", subcategories: ['Shoes or Sandals', 'Socks', 'Belt', 'Pants or Shorts', 'Shirt', 'Sweater']},
+    { name: "Essentials", iconName: "announcement", subcategories: ['Toiletries', 'Wallet', 'Phone', 'Credit Card', 'Cash'] },
+    { name: "Airport", iconName: "flight_takeoff", subcategories: ['Passport', "Driver's License", 'Luggage', 'Ticket', 'Alarm'] },
+    { name: "Clothes", iconName: "snowshoeing", subcategories: ['Shoes or Sandals', 'Socks', 'Pants or Shorts', 'Shirt', 'Sweater']},
     { name: "Electronics", iconName: "add_a_photo", subcategories: ['Camera', 'Cell Phone', 'Charger', 'Power Adapter', 'Laptop']},
     { name: "Health", iconName: "health_and_safety", subcategories: ['Medications', 'Sunblock', 'Vitamins', 'Insect Repellant', 'Toothbrush & Toothpaste']}
   ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -36,8 +36,8 @@ export class HomeComponent implements OnInit {
     {text: "Daisy Harley", imgSource: "assets/images/multi-colored-hair-testimonial.jpg", cols: 1, rows:1, color: 'lightblue'},
     {text: "If I were a real person, I would give a 5-star rating for 5-star planning!", imgSource: '', cols: 2, rows:1, color: 'white'},
     {text: "I am also not a real person, but this app is really good.", imgSource: '', cols: 2, rows:1, color: 'white'},
-    {text: "Daisy Harley", imgSource: "assets/images/multi-colored-hair-testimonial.jpg", cols: 1, rows:1, color: 'lightblue'},
-    {text: "Daisy Harley", imgSource: "assets/images/multi-colored-hair-testimonial.jpg", cols: 1, rows:1, color: 'lightblue'},
+    {text: "Daisy Harley", imgSource: "assets/images/multi-colored-hair-testimonial.jpg", cols: 1, rows:1, color: 'lightgreen'},
+    {text: "Daisy Harley", imgSource: "assets/images/multi-colored-hair-testimonial.jpg", cols: 1, rows:1, color: 'lightpurple'},
     {text: "I believe I'm real, and I know this app is fantastic!", imgSource: '', cols: 2, rows:1, color: 'white'}
   ];
 
