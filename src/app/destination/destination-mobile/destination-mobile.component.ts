4ef13f727b4db3e615adc783240028559d6c8c26
[Alwin] <[alwinnocom@gmail.com]>
2021-02-09 15:03:13 -0800

    2/9 - 5th commit - Split destination into destination-mobile + destination-desktop. CSS and TS adjusted accordingly.

--- src/app/destination/destination-mobile/destination-mobile.component.ts
+++ src/app/destination/destination-mobile/destination-mobile.component.ts
@@ -1,15 +1,147 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject, Subscription }  from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { MatAccordion } from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-destination-mobile',
   templateUrl: './destination-mobile.component.html',
   styleUrls: ['./destination-mobile.component.css']
 })
-export class DestinationMobileComponent implements OnInit {
 
-  constructor() { }
+export class DestinationMobileComponent implements OnInit, OnDestroy {
+
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+
+  vacationControl = new FormControl();
+  vacationGroups = [
+
+    {
+      name: 'Covid-19 Personal Status',
+      disabled: false,
+      vacation: [
+        {value: 'vaccinated', viewValue: 'Vaccinated', viewColor: 'green'},
+        {value: 'at-risk', viewValue: 'At-Risk'},
+        {value: 'sick', viewValue: 'Sick'}
+      ]
+    },
+
+    {
+      name: 'Purpose',
+      vacation: [
+        {value: 'travel_blog', viewValue: 'Travel Blog'},
+        {value: 'business', viewValue: 'Business'},
+        {value: 'tourist', viewValue: 'Tourist'},
+      ]
+    },
+
+    {
+      name: 'Previously Visited...',
+      vacation: [
+        {value: 'very-often', viewValue: '5-10+ times'},
+        {value: 'often', viewValue: '3-4 times'},
+        {value: 'rarely', viewValue: '1-2 times'},
+        {value: 'newcomer', viewValue: 'Never visited before.'}
+      ]
+    },
+    {
+      name: 'Interests',
+      vacation: [
+        {value: 'exploration', viewValue: 'Exploration'},
+        {value: 'culture', viewValue: 'Culture'},
+        {value: 'dining', viewValue: 'Dining'},
+        {value: 'shopping', viewValue: 'Shopping'},
+        {value: 'late-night', viewValue: 'Late-Night'},
+        {value: 'indoors', viewValue: 'Indoors'},
+        {value: 'outdoors', viewValue: 'Outdoors'},
+      ]
+    },
+    {
+      name: 'Adventure',
+      vacation: [
+        {value: "Hero's Journey", viewValue: "Hero's Journey"},
+        {value: 'adventurous', viewValue: 'Adventurous'},
+        {value: 'cautious', viewValue: 'Curious'},
+        {value: 'relaxed', viewValue: 'Relaxed'}
+      ]
+    },
+    {
+      name: 'Seasons',
+      vacation: [
+        {value: 'winter', viewValue: 'Winter'},
+        {value: 'spring', viewValue: 'Spring'},
+        {value: 'summer', viewValue: 'Summer'},
+        {value: 'autumn', viewValue: 'Autumn'}
+      ]
+    }
+  ];
+
+
+  constructor() {}
+
+  public startDateChanged = new Subject();
+  public startDateChangeSubscription: Subscription
+
+  public endDateChanged = new Subject();
+  public endDateChangeSubscription: Subscription
+
+  public departurePointChanged: Subject<string> = new Subject<string>();
+  public departurePointChangeSubscription: Subscription
+
+  public destinationChanged: Subject<string> = new Subject<string>();
+  public destinationChangeSubscription: Subscription
+
+  start_date = null;
+  end_date = null;
+
+  departure_point: string = "";
+  destination: string = "";
+
+  return_via: string = "";
+  return_point: string = "";
+
 
   ngOnInit(): void {
+
+    this.startDateChangeSubscription = this.startDateChanged
+      .pipe(
+        debounceTime(1000),
+      )
+      .subscribe(startDate => {
+        this.start_date = startDate;
+      });
+
+    this.endDateChangeSubscription = this.endDateChanged
+      .pipe(
+        debounceTime(1000),
+      )
+      .subscribe(endDate => {
+        this.end_date = endDate;
+      });
+
+    this.departurePointChangeSubscription = this.departurePointChanged
+      .pipe(
+        debounceTime(1000),
+      )
+      .subscribe(departurePoint => {
+        this.departure_point = departurePoint;
+      });
+
+    this.destinationChangeSubscription = this.destinationChanged
+      .pipe(
+        debounceTime(1000),
+      )
+      .subscribe(destination => {
+        this.destination = destination;
+      });
   }
 
+  ngOnDestroy() {
+    this.startDateChangeSubscription.unsubscribe();
+    this.endDateChangeSubscription.unsubscribe();
+    this.departurePointChangeSubscription.unsubscribe();
+  }
 }
--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Subject, Subscription }  from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-
-import { MatAccordion } from '@angular/material/expansion';
+import { Component, OnInit } from '@angular/core';
 
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+
 
 @Component({
   selector: 'app-destination',
@@ -14,97 +9,10 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
   styleUrls: ['./destination.component.css']
 })
 
-export class DestinationComponent implements OnInit, OnDestroy {
-
-  @ViewChild(MatAccordion) accordion: MatAccordion;
-
-  vacationControl = new FormControl();
-  vacationGroups = [
-
-    {
-      name: 'Covid-19 Personal Status',
-      disabled: false,
-      vacation: [
-        {value: 'vaccinated', viewValue: 'Vaccinated', viewColor: 'green'},
-        {value: 'at-risk', viewValue: 'At-Risk'},
-        {value: 'sick', viewValue: 'Sick'}
-      ]
-    },
-
-    {
-      name: 'Purpose',
-      vacation: [
-        {value: 'travel_blog', viewValue: 'Travel Blog'},
-        {value: 'business', viewValue: 'Business'},
-        {value: 'tourist', viewValue: 'Tourist'},
-      ]
-    },
-
-    {
-      name: 'Previously Visited...',
-      vacation: [
-        {value: 'very-often', viewValue: '5-10+ times'},
-        {value: 'often', viewValue: '3-4 times'},
-        {value: 'rarely', viewValue: '1-2 times'},
-        {value: 'newcomer', viewValue: 'Never visited before.'}
-      ]
-    },
-    {
-      name: 'Interests',
-      vacation: [
-        {value: 'exploration', viewValue: 'Exploration'},
-        {value: 'culture', viewValue: 'Culture'},
-        {value: 'dining', viewValue: 'Dining'},
-        {value: 'shopping', viewValue: 'Shopping'},
-        {value: 'late-night', viewValue: 'Late-Night'},
-        {value: 'indoors', viewValue: 'Indoors'},
-        {value: 'outdoors', viewValue: 'Outdoors'},
-      ]
-    },
-    {
-      name: 'Adventure',
-      vacation: [
-        {value: "Hero's Journey", viewValue: "Hero's Journey"},
-        {value: 'adventurous', viewValue: 'Adventurous'},
-        {value: 'cautious', viewValue: 'Curious'},
-        {value: 'relaxed', viewValue: 'Relaxed'}
-      ]
-    },
-    {
-      name: 'Seasons',
-      vacation: [
-        {value: 'winter', viewValue: 'Winter'},
-        {value: 'spring', viewValue: 'Spring'},
-        {value: 'summer', viewValue: 'Summer'},
-        {value: 'autumn', viewValue: 'Autumn'}
-      ]
-    }
-  ];
-
+export class DestinationComponent implements OnInit {
 
   public mobileSize: boolean;
   constructor(public breakpointObserver: BreakpointObserver) {}
-
-  public startDateChanged = new Subject();
-  public startDateChangeSubscription: Subscription
-
-  public endDateChanged = new Subject();
-  public endDateChangeSubscription: Subscription
-
-  public departurePointChanged: Subject<string> = new Subject<string>();
-  public departurePointChangeSubscription: Subscription
-
-  public destinationChanged: Subject<string> = new Subject<string>();
-  public destinationChangeSubscription: Subscription
-
-  start_date = null;
-  end_date = null;
-
-  departure_point: string = "";
-  destination: string = "";
-
-  return_via: string = "";
-  return_point: string = "";
 
 
   ngOnInit(): void {
@@ -118,43 +26,5 @@ export class DestinationComponent implements OnInit, OnDestroy {
           this.mobileSize = false;
         }
       });
-
-    this.startDateChangeSubscription = this.startDateChanged
-      .pipe(
-        debounceTime(1000),
-      )
-      .subscribe(startDate => {
-        this.start_date = startDate;
-      });
-
-    this.endDateChangeSubscription = this.endDateChanged
-      .pipe(
-        debounceTime(1000),
-      )
-      .subscribe(endDate => {
-        this.end_date = endDate;
-      });
-
-    this.departurePointChangeSubscription = this.departurePointChanged
-      .pipe(
-        debounceTime(1000),
-      )
-      .subscribe(departurePoint => {
-        this.departure_point = departurePoint;
-      });
-
-    this.destinationChangeSubscription = this.destinationChanged
-      .pipe(
-        debounceTime(1000),
-      )
-      .subscribe(destination => {
-        this.destination = destination;
-      });
-  }
-
-  ngOnDestroy() {
-    this.startDateChangeSubscription.unsubscribe();
-    this.endDateChangeSubscription.unsubscribe();
-    this.departurePointChangeSubscription.unsubscribe();
   }
 }
