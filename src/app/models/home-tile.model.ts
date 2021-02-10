35f52c3c5b3860b79e306c4210f4a418f4a291bb
[Alwin] <[alwinnocom@gmail.com]>
2021-02-09 18:42:42 -0800

    2/9 - 17th commit - Remove background colors from home-testimonial + simplify home-landing-page figcaption.

--- src/app/models/home-tile.model.ts
+++ src/app/models/home-tile.model.ts
@@ -1,6 +1,6 @@
 // Model from Angular Material Documentation for Grid-List-Tile = https://material.angular.io/components/grid-list/examples
 export interface Tile {
-  color: string;
+  color?: string;
   cols: number;
   rows: number;
   text: string;
