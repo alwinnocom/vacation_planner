3419e486ce1e47256a4f7479ad7a592b8c942c03
[Alwin] <[alwinnocom@gmail.com]>
2021-02-25 11:57:34 -0800

    4th commit - localhost:8080 & node server.js work! server.js code was updated to make it work with './angular-build with NO package.json reference. Additionally, package.json keeps app as its name.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/vacation_planner.json'));
+app.use(express.static('./angular-build'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/vacation_planner.json/'}
+    res.sendFile('index.html', {root: '/angular-build/'}
   );
 });
 
