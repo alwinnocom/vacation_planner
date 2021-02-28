212f0648d0b245074bffe56c5735f712c7803b7e
[Alwin] <[alwinnocom@gmail.com]>
2021-02-27 22:15:11 -0800

    2/27 - 5th and Last Commit for the time being. The project is ready to be presented. It is not fully designed, but after the Internship, you will be a more mature programmer and can finish the project. Right now, treat the Project like the Trivia Project. You came back to it after 3 months and added finishing touches to the project. Right now, it is important to recognize that the project, although not completely ready, is still presentable as an Angular, Angular Material, RxJS, Typescript, & Figma Project. You have 3 Projects on the Portfolio now, so focus on Leetcode.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,8 @@ import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 export class HeaderComponent implements OnInit {
 
   public mobileSize: boolean;
+  public tabletSize: boolean;
+
   constructor(public breakpointObserver: BreakpointObserver) {}
 
   ngOnInit(): void {
@@ -19,6 +21,16 @@ export class HeaderComponent implements OnInit {
           this.mobileSize = true;
         } else {
           this.mobileSize = false;
+        }
+      });
+
+      this.breakpointObserver
+      .observe(['(max-width: 1000px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.tabletSize = true;
+        } else {
+          this.tabletSize = false;
         }
       });
   }
